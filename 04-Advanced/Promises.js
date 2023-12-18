a4e2f4d76f7eff984e9c58a9577cd9818fe7b3f0
Jyotiprakash Mohapatra <152247373+JyotiprakashMohapatra07@users.noreply.github.com>
2023-12-18 16:19:54 +0000

    File added

---
+++ 04-Advanced/Promises.js
@@ -0,0 +1,123 @@
+// var ans=new Promise((Res,Rej)=> {
+// if(true){
+// return Res();
+// }
+// else{
+//     return Rej();
+// }
+// })
+// ans.then(function(){
+//     console.log("resolve the condition");
+// })
+// .catch(function(){
+//     console.log("rejected the comdition");
+// })
+
+ /*
+1.User will ask for a number between 0 to 9 and if the number is below 5 resolve of 
+not reject.
+2.i.reach the house
+ii.open the gate
+iii.prepare food and have that
+iv.scroll instagram reels
+
+*/
+//1.Answer
+// var num =0;
+// var ans=new Promise((res,rej)=>{
+//     if(num<5){
+//         return res();
+//     }
+//     else{
+//         return rej();
+//     }
+// })
+// ans.then(function(){
+//     console.log("Resolve the condition");
+// })
+// .catch(function(){
+//     console.log("Reject the condition");
+// })
+
+
+// 2.Answer
+
+//  var ans=new Promise(function(res,rej){
+//     return res("reach the house");
+//  })
+//  var p2=ans.then(function(data){
+// console.log(data);
+// return new Promise(function(res,rej){
+//     return res("open the gate")
+// })
+//  })
+//  var p3=p2.then(function(data){
+//     console.log(data);
+//     return new Promise(function(res,rej){
+//         return res("prepare food and have that")
+
+//     })
+//  })
+//  var p4 =p3.then(function(data){
+//     console.log(data);
+//     return new Promise(function(res,rej){
+//         return res("scroll instagram reels")
+//     })
+//  })
+//  var p5=p4.then(function(data){
+//     console.log(data);
+
+//  })
+
+
+// const promiseEx3=new Promise(function(res,rej){
+//     setTimeout(function(){
+//         let error=true
+//    if(!error){
+//     res({username:"Jyoti",password:"1234"})
+//    }
+//    else{
+//     rej('ERROR:Something went wrong.page is not found')
+//    }
+//     },2000)
+// })
+// async function NewpromiseEx3(){
+//     try{
+//     const response= await promiseEx3
+//     console.log(response);
+//     }catch (error){
+//         console.log(error);
+//     }
+// }
+// NewpromiseEx3(
+// )
+// promiseEx3.then((user)=>{
+// console.log(user);
+// return user.username
+// }).then((username)=>{
+//     console.log(username);
+// }).catch(function(error){
+// console.log(error);
+// })
+
+
+// async function getAllUsers(){
+//     try{
+//         const response=await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data=await response.json()
+//         console.log(data);
+//     }catch (error){
+//         console.log("E:",error);
+//     }
+// }
+// getAllUsers()
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>console.log(error))
